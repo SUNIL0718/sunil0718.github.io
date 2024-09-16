d7b611be32b3ae01a26d5336a5424d343a79607a
SUNIL0718 <skay0718@gmail.com>
2024-09-16 17:15:26 +0530

    project file added

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -42,5 +43,45 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-bold tracking-widest bg-gradient-to-t from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            Real-world Projects
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 m-4">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative after:content-[''] after:absolute after:inset-0"
+            >
+              <div>
+                <span>{project.company}</span>
+                <span>{project.year}</span>
+              </div>
+              <h3>{project.title}</h3>
+              <hr />
+              <ul>
+                {project.results.map((results) => (
+                  <li key={results.title}>{results.title}</li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button>View Live Site</button>
+              </a>
+              <Image src={project.image} alt={project.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
